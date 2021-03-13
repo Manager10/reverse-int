1269d0f3d4215b7a0730233450f05673ea14dfc4
Manager10 <72505775+Manager10@users.noreply.github.com>
2021-03-13 16:07:31 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    if (n < 0)
+        n *= -1;
+
+    n = String(n);
+    let temp = '';
+    let j = n.length - 1;
+    for (let i = 0; i < n.length; i++) {
+        temp += n[j];
+        j--;
+    }
+
+    temp -= '';
+    return temp;
 }
